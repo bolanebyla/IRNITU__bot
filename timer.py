e60d295070b772f0e92cebdac16947b8fcbd0621
bolanebyla <alexleskov2000@gmail.com>
2019-11-10 19:10:50 +0800

    Сделал для двух занятий

--- timer.py
+++ timer.py
@@ -5,14 +5,12 @@ import pytz
 
 global TZ_IRKUTSK
 TZ_IRKUTSK = pytz.timezone('Asia/Irkutsk') # Часовой пояс
-print (TZ_IRKUTSK)
 
-def timer_otrabotka(data='', time_zan = ''):
+def timer_otrabotka(data='', time_lesson = ''):
     global TZ_IRKUTSK
-
-    if data == None or time_zan == None:
-        return ''
     
+    time_lesson = time_lesson.split(':')
+
     now = datetime.now(TZ_IRKUTSK)
     year = int(now.strftime('%Y'))
     month = int(now.strftime('%m'))
@@ -23,23 +21,29 @@ def timer_otrabotka(data='', time_zan = ''):
     now = datetime(year, month, day,h, m, s) # Текущее время
 
     data = data.split('.')
-    otrabotka = datetime(int(data[2]), int(data[1]), int(data[0]), int(time_zan[0]),int(time_zan[1]))
+    otrabotka = datetime(int(data[2]), int(data[1]), int(data[0]), int(time_lesson[0]),int(time_lesson[1]))
+    time_lesson = time_lesson[0] + ':' + time_lesson[1]
     period = otrabotka - now # Сколько осталось
 
     mm, ss = divmod(period.seconds, 60)
     hh, mm = divmod(mm, 60)
-    print('Осталось: {} д {} ч {} мин {} сек.'.format(period.days, hh, mm, ss))
+    
     if str(period)[0] == '-':
-        return ''
-    return 'До отработки осталось: {} д {} ч {} мин {} сек.'.format(period.days, hh, mm, ss)
+        return 'Ближайших отработок нет'
+    msg = 'Отработка состоится: {} ({}) в {} (через {} д {} ч {} мин {} сек)'.format(otrabotka.strftime("%d.%m.%Y"), otrabotka.strftime("%A"), time_lesson, period.days, hh, mm, ss)
+    print (msg)
+    return msg
 
-print(timer_otrabotka('09.11.2019', '15:00'))
+timer_otrabotka('11.11.2019', '15:00')
 
-def timer_lesson(weekday_zan='', time_zan=''):
-    time_zan = time_zan.split(':') # Время занятия 
+def timer_lesson(weekday_lesson1='', time_lesson1='', weekday_lesson2='', time_lesson2=''):
+    time_lesson1 = time_lesson1.split(':') # Время занятия 1
+    time_lesson2 = time_lesson2.split(':') # Время занятия 2
+    weekday_lessons = (weekday_lesson1, weekday_lesson2)
+    time_lessons = (time_lesson1, time_lesson2)
 
     global TZ_IRKUTSK
-    weekday = ['пн','вт','ср','чт','пт','сб','вс']
+    weekday = ('пн','вт','ср','чт','пт','сб','вс')
 
     now = datetime.now(TZ_IRKUTSK)
     year = int(now.strftime('%Y'))
@@ -50,48 +54,57 @@ def timer_lesson(weekday_zan='', time_zan=''):
     s = int(now.strftime('%S'))
     now = datetime(year, month, day,h, m, s) # Текущее время
     print(now.strftime("%d %B %Y (%a)")) 
-
-    weekday_now = (now.strftime('%a')).lower()
-    print(weekday_now)
+    weekday_now = (now.strftime('%a')).lower() # Текущий день недели
 
 
+    data  = [{'k': 0, 'time_lesson': time_lesson1}, {'k': 0, 'time_lesson': time_lesson2}]
+    best_distance = 8
+    time_lesson = time_lesson1
+    weekday_lesson = weekday_lesson1
 
-    # Определяем сколько дней до ближайшего занятия
-    if int(time_zan[0])<=h and int(time_zan[1])<=m:
-        k=8
-    else:
-        print('зашло')
-        f = False
-        k=0
-        for i in weekday:
-    
-            k+=1
-            if i == weekday_now:
-                f = True
-                k=0
+    for m in range (2):
+        # Определяем сколько дней до ближайшего занятия
+        if weekday_now == weekday_lessons[m] and int(time_lessons[m][0])<=h and int(time_lessons[m][1])<=m:
+            k=8
+        else:
+            f = False
+            k=0
+            for i in weekday:
                 k+=1
-            if i == weekday_zan and f == True :
-                break
-            if i == 'вс' and k!=0:
-                for j in weekday:
+                if i == weekday_now:
+                    f = True
+                    k=0
                     k+=1
-                    if j == weekday_zan:
-                        break
+                if i == weekday_lessons[m] and f == True :
+                    k-=1
+                    break
+                if i == 'вс' and k!=0:
+                    for j in weekday:
+                        k+=1
+                        if j == weekday_lessons[m]:
+                            k-=1
+                            break
+        data[m]['k'] = k
+        print(k)
+        if data [m]['k'] < best_distance:
+            best_distance = data[m]['k']
+            time_lesson = data[m]['time_lesson']
+            weekday_lesson = weekday_lessons[m]
 
-
-    zanatie_date = now + timedelta(k-1)
+    zanatie_date = now + timedelta(best_distance)
     year = int(zanatie_date.strftime('%Y'))
     month = int(zanatie_date.strftime('%m'))
     day = int(zanatie_date.strftime('%d'))
-    zanatie = datetime(year, month, day,int(time_zan[0]),int(time_zan[1])) # Дата и время ближайшего занятия
-
+    zanatie = datetime(year, month, day,int(time_lesson[0]),int(time_lesson[1])) # Дата и время ближайшего занятия
+    time_lesson = time_lesson[0] + ':' + time_lesson[1]
     print(zanatie.strftime("%d %B %Y (%a)")) 
 
     period = zanatie - now # Сколько осталось
 
     mm, ss = divmod(period.seconds, 60)
     hh, mm = divmod(mm, 60)
-    print('До занятия осталось: {} д {} ч {} мин {} сек.'.format(period.days, hh, mm, ss))
-    return 'До занятия осталось: {} д {} ч {} мин {} сек.'.format(period.days, hh, mm, ss)
+    msg = 'Следующее занятие состоится: {} ({}) в {} (через {} д {} ч {} мин {} сек)'.format(zanatie.strftime("%d.%m.%Y"),zanatie.strftime("%A"), time_lesson, period.days, hh, mm, ss)
+    print(msg)
+    return msg
 
-
+timer_lesson(weekday_lesson1='чт', time_lesson1='19:00', weekday_lesson2='сб',  time_lesson2='15:00')
